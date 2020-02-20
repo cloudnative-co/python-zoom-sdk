f7e76a1041242cd2a8031ac829a3fe257be66ba7
Sebastian <seba@cloudnative.co.jp>
2020-02-20 06:30:07 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="PythonSDK",
+    version="0.0.1",
+    description="Python SDK for Python 3.6",
+    author="sebastian",
+    author_email="seba@cloudnative.co.jp",
+    packages=find_packages(),
+    install_requires=[
+    ],
+    entry_points={
+        "console_scripts": [
+        ]
+    },
+)
